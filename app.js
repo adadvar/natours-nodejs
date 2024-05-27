9d6482d432b90d9ee728296893a3f83bf6fb8252
adadvar <alirezadadvar24@gmail.com>
2024-05-27 09:39:42 +0330

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  res.send('Done');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
